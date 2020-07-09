6e923d0fdfd9e2ef99c0c922b56da6924f37305a
zhangliangliang <bluecard*liang>
2020-07-09 13:12:00 +0800

    day04-1

---
+++ arrow-offer/src/main/java/com/leetcode/arrow/offer/Solution008.java
@@ -0,0 +1,46 @@
+package com.leetcode.arrow.offer;
+
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 2
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * 输入：n = 7
+ * 输出：21
+ * 提示：
+ * <p>
+ * 0 <= n <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution008 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(7));
+
+    }
+
+    public static int solution(int n) {
+        /**
+         * 类似于斐波那契数列
+         * 即 f(0) = 1, f(1) = 1, f(2) = 2,f(3) = f(1)+f(2),f(n) = f(n-2)+f(n-1)
+         * 所以可以使用动态规划方法解决
+         */
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}

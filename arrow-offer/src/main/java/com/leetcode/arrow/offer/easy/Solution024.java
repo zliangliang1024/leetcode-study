4d770b40e295a5c5e6283c5be76f8a52c9b0304b
zhangliangliang <bluecard*liang>
2020-07-30 17:27:32 +0800

    preparing day14

---
+++ arrow-offer/src/main/java/com/leetcode/arrow/offer/easy/Solution024.java
@@ -0,0 +1,34 @@
+package com.leetcode.arrow.offer.easy;
+
+/**
+ * 剑指 Offer 40. 最小的k个数
+ * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8 这8个数字，则最小的4个数字是1、2、3、4。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [3,2,1], k = 2
+ * 输出：[1,2] 或者 [2,1]
+ * 示例 2：
+ * <p>
+ * 输入：arr = [0,1,2,1], k = 1
+ * 输出：[0]
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= k <= arr.length <= 10000
+ * 0 <= arr[i] <= 10000
+ */
+public class Solution024 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] getLeastNumbers(int[] arr, int k) {
+        return new int[0];
+
+    }
+
+}

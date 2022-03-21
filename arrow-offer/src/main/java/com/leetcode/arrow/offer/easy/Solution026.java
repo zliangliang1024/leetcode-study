85165873cf027ac55f7d2a247f52ef6e53d30f9d
yao0er4 <zliangliang1024@126.com>
2022-03-21 23:02:17 +0800

    2022-03-21:剑指 Offer 55 - I. 二叉树的深度

---
+++ arrow-offer/src/main/java/com/leetcode/arrow/offer/easy/Solution026.java
@@ -0,0 +1,83 @@
+package com.leetcode.arrow.offer.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 55 - I. 二叉树的深度
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度3 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 节点总数 <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution026 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 深度优先遍历（DFS）
+     * @param root
+     * @return
+     */
+    public int maxDepth1(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1;
+    }
+
+    /**
+     * 广度优先遍历(BFS)
+     * @param root
+     * @return
+     */
+    public int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            // 层级遍历
+            res++;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
